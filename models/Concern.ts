708f13333390dae79962798460892fed8ff38d08
itsmenewbie03 <2101102516@student.buksu.edu.ph>
2023-10-26 22:52:22 +0800

    implemented TE-34

---
+++ models/Concern.ts
@@ -0,0 +1,18 @@
+import { ObjectId } from "mongodb";
+type ConcernStatus =
+    | "Open"
+    | "In Progress"
+    | "Closed"
+    | "Resolved"
+    | "Reopened";
+
+// Usage example
+
+interface Concern {
+    message: string;
+    transaction_id?: ObjectId;
+    date: Date;
+    user_id: ObjectId;
+    status: ConcernStatus;
+}
+export type { Concern };
