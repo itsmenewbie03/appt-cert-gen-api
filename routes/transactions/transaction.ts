d58cc5f398fa61660166c86dc03c4794447f4a66
itsmenewbie03 <2101102516@student.buksu.edu.ph>
2023-11-22 22:14:51 +0800

    fix(appointments): update the correct middleware
    This commit fixed the mistake in putting the auth middleware instead of priviledge user auth
    
    using neovim xD

--- routes/transactions/transaction.ts
+++ routes/transactions/transaction.ts
@@ -4,12 +4,17 @@ import {
   appointment_list_controller,
   create_appointment_controller,
 } from "../../controllers/transactions/appointment_controllers";
+import { privileged_user_auth } from "../../middlewares/privileged_user_auth";
 const transaction_router = Router();
 
 transaction_router.use(json());
 transaction_router.post("/create", auth, create_appointment_controller);
 // NOTE: for this endpoint is for admin,
 // a separate endpoint for retreiving personal appointments will be added
-transaction_router.get("/list", auth, appointment_list_controller);
+transaction_router.get(
+  "/list",
+  privileged_user_auth,
+  appointment_list_controller,
+);
 
 export default transaction_router;
