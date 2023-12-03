f5de0c065b6b0022ac9e94646afac80379c19c96
itsmenewbie03 <2101102516@student.buksu.edu.ph>
2023-12-03 23:24:05 +0800

    feat(documents): add document upload endpoint
    This commit aims to resolve #1. This needs more work
    
    using neovim xD

--- controllers/documents/document_controllers.ts
+++ controllers/documents/document_controllers.ts
@@ -13,4 +13,10 @@ const document_list_controller = async (req: Request, res: Response) => {
     .json({ message: "Documents retrieved successfully", data: documents });
 };
 
-export { document_list_controller };
+const document_create_controller = async (req: Request, res: Response) => {
+  const body = req.body;
+  console.log("YAY we GOT:", JSON.stringify(body));
+  return res.status(200).json(body);
+};
+
+export { document_list_controller, document_create_controller };
--- routes/documents/document.ts
+++ routes/documents/document.ts
@@ -1,11 +1,19 @@
 import { Router, json } from "express";
-import { document_list_controller } from "../../controllers/documents/document_controllers";
+import {
+  document_create_controller,
+  document_list_controller,
+} from "../../controllers/documents/document_controllers";
 import { auth } from "../../middlewares/auth";
+import { privileged_user_auth } from "../../middlewares/privileged_user_auth";
 
 const document_router = Router();
-document_router.use(json());
-document_router.use(auth);
+document_router.use(json({ limit: "5mb" }));
 
-document_router.get("/list", document_list_controller);
+document_router.get("/list", auth, document_list_controller);
+document_router.post(
+  "/create",
+  // privileged_user_auth,
+  document_create_controller,
+);
 
 export default document_router;
