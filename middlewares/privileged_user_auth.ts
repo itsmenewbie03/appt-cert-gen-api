d8b028ed7cab3b6d58da737f04e4fb0f082d19ed
itsmenewbie03 <2101102516@student.buksu.edu.ph>
2024-05-08 11:17:48 +0800

    feat: partial support firebase auth token

--- middlewares/privileged_user_auth.ts
+++ middlewares/privileged_user_auth.ts
@@ -1,5 +1,6 @@
 import type { Request, Response, NextFunction } from "express";
 import { verify_token } from "../utils/jwt";
+import { fireabase_verify_token } from "../utils/firebase_auth";
 
 const privileged_user_auth = async (
   req: Request,
@@ -28,6 +29,26 @@ const privileged_user_auth = async (
   const token_data = await verify_token(token, "access_token").catch(
     (err) => err.code,
   );
+  // NOTE: we will check if token_data is undefined
+  // this due to us integrating Firebase Auth in the client side
+  if (!token_data) {
+    console.log(":: DEBUG (TOKEN_DATA) priv_user_auth: ", token_data);
+    const firebase_verify_result = await fireabase_verify_token(token);
+    if (!firebase_verify_result) {
+      return res.status(403).json({
+        message: "So you're a bit of a hacker huh? You can do better.",
+      });
+    }
+    // WARN: this is a temporary solution
+    // we need a better way like implement a database
+    // in which a firebase uid is mapped to their respective role
+    req.headers["role"] = "admin";
+    req.headers["email"] = firebase_verify_result.email;
+    next();
+    // NOTE: not sure if next causes the function to return
+    // so we will do an early return
+    return;
+  }
   // check if ERR_JWS_INVALID || ERR_JWT_EXPIRED || ERR_JWS_SIGNATURE_VERIFICATION_FAILED
   if (error_codes.includes(token_data)) {
     return res.status(403).json({
