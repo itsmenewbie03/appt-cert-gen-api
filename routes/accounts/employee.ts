e5ac86cbe1fc43219f8d808a20272c31c2b0640d
itsmenewbie03 <2101102516@student.buksu.edu.ph>
2024-05-17 19:24:08 +0800

    feat: register `change_avatar` and `info` route
    This registers two new routes the calls the newly created controllers in
    31ff2f4.

--- routes/accounts/employee.ts
+++ routes/accounts/employee.ts
@@ -1,11 +1,13 @@
 import { Router, json } from 'express';
 import { admin_auth } from '../../middlewares/admin_auth';
+import { privileged_user_auth } from '../../middlewares/privileged_user_auth';
 import {
+  employee_avatar_update_controller,
   employee_change_password_controller,
+  employee_data_controller,
   employee_delete_controller,
   employee_update_controller,
 } from '../../controllers/auth/employee_controllers';
-import { auth } from '../../middlewares/auth';
 
 const employee_accounts_router = Router();
 
@@ -17,11 +19,36 @@ employee_accounts_router.delete(
   employee_delete_controller,
 );
 
-employee_accounts_router.patch('/update', auth, employee_update_controller);
+employee_accounts_router.patch(
+  '/update',
+  privileged_user_auth,
+  employee_update_controller,
+);
+
+// NOTE: this is to support android client
+employee_accounts_router.post(
+  '/update',
+  privileged_user_auth,
+  employee_update_controller,
+);
 
 employee_accounts_router.patch(
   '/change_password',
-  auth,
+  privileged_user_auth,
   employee_change_password_controller,
 );
+
+// NOTE: this will handle the new avatar feature
+employee_accounts_router.post(
+  '/change_avatar',
+  privileged_user_auth,
+  employee_avatar_update_controller,
+);
+
+employee_accounts_router.get(
+  '/info',
+  privileged_user_auth,
+  employee_data_controller,
+);
+
 export default employee_accounts_router;
